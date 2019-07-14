e6faec139c85d0ac1a6f28aa12e647ec9599792e
bahrambds <bahrambds@gmail.com>
2019-07-14 01:57:53 -0400

    update

--- buenosfiles/__init__.py
+++ buenosfiles/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
-from buenosfiles.config import Config
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
 
     from buenosfiles.errors.handlers import errors
